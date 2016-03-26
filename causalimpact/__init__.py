b1beddc7e5d13c40cf0d60261618e21b1d320736
Jamal Senouci <jamalsenouci@gmail.com>
2016-03-26 11:26:14 +0800

    scaffold package structure

---
+++ causalimpact/__init__.py
@@ -0,0 +1,9 @@
+"""
+Causal Impact
+=============
+
+  A Python package for causal inference using Bayesian structural time-series models. It's a port of the R package CausalImpact, see https://github.com/google/CausalImpact
+  
+"""
+
+from causalimpact.tests.test import run as test

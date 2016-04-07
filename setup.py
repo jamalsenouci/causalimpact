abb8ce913c76b20d4d2405ad7b5c44884bc951e7
jamal <jamalsenouci@gmail.com>
2016-04-07 20:58:30 +0100

    added package files Manifest setup and requirements as well as travis-ci file

--- setup.py
+++ setup.py
@@ -1,20 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Setup script for causalimpact
+You can install causalimpact with
+python setup.py install
+"""
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+import sys
+
+if sys.argv[-1] == 'setup.py':
+    print("To install, run 'python setup.py install'")
+    print()
+
+if sys.version_info[:2] < (2, 6):
+    print("CausalImpact requires Python 2.6 or later (%d.%d detected)." %
+          sys.version_info[:2])
+    sys.exit(-1)
+
+
+packages=["causalimpact",
+          "causalimpact.tests"]
+
+# add the tests
+package_data     = {
+    'causalimpact': ['tests/*.py'],
+    }
+
 config = {
-    'description': 'My Project',
-    'author': 'My Name',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'name': 'causalimpact'
+    'description': 'Python Package for causal inference using Bayesian\
+                    structural time-series models',
+    'author': 'Jamal Senouci',
+    'url': 'http://jamalsenouci.github.io/causalimpact.html',
+    'download_url': 'https://pypi.python.org/pypi/causalimpact/',
     'version': '0.1',
+    'platforms': ['Linux','Mac OSX','Windows','Unix'],
     'install_requires': ['nose'],
     'packages': ['CausalImpact'],
-    'scripts': [],
-    'name': 'projectname'
+    'test_suite: 'nose.collector',
+    'tests_require': ['nose>=0.10.1'],
+    'packages': packages,
+    'package_data': package_data
 }
 
 setup(**config)
+
+
+if __name__ == "__main__":
+
+    setup(
+        name             = release.name.lower(),
+        version          = version,
+        maintainer       = release.maintainer,
+        maintainer_email = release.maintainer_email,
+        author           = release.authors['Hagberg'][0],
+        author_email     = release.authors['Hagberg'][1],
+        description      = release.description,
+        keywords         = release.keywords,
+        long_description = release.long_description,
+        license          = release.license,
+        platforms        = release.platforms,
+        url              = release.url,
+        download_url     = release.download_url,
+        classifiers      = release.classifiers,
+        packages         = packages,
+        data_files       = data,
+        package_data     = package_data,
+        install_requires = install_requires,
+        test_suite       = 'nose.collector',
+        tests_require    = ['nose>=0.10.1'],
+        zip_safe         = False
+    )

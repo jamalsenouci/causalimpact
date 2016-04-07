8e6702c6ca85f617d0ce9ce2783d5a419ae1b730
jamal <jamalsenouci@gmail.com>
2016-04-07 21:52:07 +0100

    updated the test runner test.py

--- causalimpact/tests/test.py
+++ causalimpact/tests/test.py
@@ -1,2 +1,41 @@
-def run():
-    pass
+#!/usr/bin/env python
+import sys
+from os import path, getcwd
+
+
+def run(verbosity=1, doctest=False, numpy=True):
+    """Run causalimpact tests.
+    Parameters
+    ----------
+    verbosity: integer, optional
+      Level of detail in test reports.  Higher numbers provide  more detail.
+    doctest: bool, optional
+      True to run doctests in code modules
+    numpy: bool, optional
+      True to test modules dependent on numpy
+    """
+    try:
+        import nose
+    except ImportError:
+        raise ImportError(
+            "The nose package is needed to run the NetworkX tests.")
+
+    sys.stderr.write("Running causalimpact tests:")
+    ci_install_dir = path.join(path.dirname(__file__), path.pardir)
+    # stop if running from source directory
+    if getcwd() == path.abspath(path.join(ci_install_dir, path.pardir)):
+        raise RuntimeError("Can't run tests from source directory.\n"
+                           "Run 'nosetests' from the command line.")
+
+    argv = [' ', '--verbosity = %d' % verbosity,
+            '-w', ci_install_dir,
+            '-exe']
+    if doctest:
+        argv.extend(['--with-doctest', '--doctest-extension=txt'])
+    if not numpy:
+        argv.extend(['-A not numpy'])
+
+    nose.run(argv=argv)
+
+if __name__ == "__main__":
+    run()
